26b802f5d2f0587af25f5c14093c3235b295d4ae
timmywimmy <timothykwok95@gmail.com>
2023-04-19 17:32:32 +0800

    initialised firebase and authcontext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,16 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
-// import { AuthProvider } from "src/auth/useAuth";
+import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (<>
+  return (<AuthProvider>
   <Head>
     <title>Home Sweet Home</title>
     <link rel="shortcut icon" href="/favicon.ico" />
   </Head>
   <Component {...pageProps} />
-  </>
+  </AuthProvider>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,17 @@
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useAuth } from "src/auth/useAuth";
 
 interface IProps{
     main: ReactNode;
 }
 
 const Layout: FunctionComponent<IProps> = ({main}) => {
-    const authenticated = false;
-    const logout = () => {
-        return console.log('hi')
-    }
+    const {logout, authenticated} = useAuth()
+    // const authenticated = false;
+    // const logout = () => {
+    //     return console.log('hi')
+    // }
 
     return (
     <div className = 'bg-gray-900 max-w-screen-2xl mx-auto text-white'>
@@ -18,7 +19,7 @@ const Layout: FunctionComponent<IProps> = ({main}) => {
             <div className="px-6 flex items-center justify-between h-16">
                 <Link href="/auth"><a> <img className="inline w-6" src="/home-color.svg" alt="home house" /></a></Link>
                 {authenticated ? <><Link href="/houses/add">Add House</Link>
-                <button onClick={() => logout()}>Logout</button></> : <Link href='/auth'><a>Login / Signup</a></Link>}
+                <button onClick={logout}>Logout</button></> : <Link href='/auth'><a>Login / Signup</a></Link>}
             </div>
         </nav>
         <main style ={{minHeight: 'calc(100vh - 64px)'}}>{main}</main>
